2e61ed5c5d1ce2ddf199b6bc5c667f883d1dbcf7
carlosyamakawa <carlos.yamakawa@gmail.com>
2020-05-08 17:30:19 -0400

    null undefined

---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,14 @@
+let valor // não inicializada
+console.log(valor) //undefined
+
+//console.log(valor2)
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+//delete produto.preco
+
