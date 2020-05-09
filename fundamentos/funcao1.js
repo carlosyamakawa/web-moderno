455470aa04b77ca7fea52c0915234e6117af7ecc
carlosyamakawa <carlos.yamakawa@gmail.com>
2020-05-09 19:18:04 -0400

    funcao

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,16 @@
+// Funcao sem retorno
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+imprimirSoma(2, 10, 3, 4, 5, 6)
+
+// Funcao com retorno
+function soma(a, b = 0) {
+    return a + b;
+}
+
+console.log(soma(2, 3))
+console.log(soma(2))
