1ecbf7e231ff15582e1e589cce58de4c268ac1c7
carlosyamakawa <carlos.yamakawa@gmail.com>
2020-05-09 19:54:43 -0400

    fundamentos

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,9 @@
+console.log(a)
+var a=2 //hoisting = içamento, ou seja, é como se jogasse a declaração no topo do código
+console.log(a)
+
+/*
+console.log(b)
+let b=3 //let não tem hoisting
+console.log(b)
+*/
