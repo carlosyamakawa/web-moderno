397d3637e7296f23a0b8f678f8f8db1dcf7e3c08
carlosyamakawa <carlos.yamakawa@gmail.com>
2020-05-09 19:33:25 -0400

    var e let em loop

---
+++ fundamentos/usandoLet1.js
@@ -0,0 +1,17 @@
+var numero = 1
+{
+    let numero = 2
+    console.log('dentro = ', numero) //2
+}
+console.log('fora = ', numero) //1
+
+
+/* 
+    var
+    - escopo global
+    - escopo funcao
+    var
+    - escopo global
+    - escopo funcao
+    - escopo bloco
+*/
