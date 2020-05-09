d5335a5282f6ec62f0f23f1bad01391e6163b93a
carlosyamakawa <carlos.yamakawa@gmail.com>
2020-05-09 19:24:22 -0400

    usandoVar

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,18 @@
+{
+    {
+        {
+            {
+                var sera = "Será?"
+            }
+        }
+    }
+}
+console.log(sera)
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+//apenas variáveis fora de funções são globais
+//console.log(local)
