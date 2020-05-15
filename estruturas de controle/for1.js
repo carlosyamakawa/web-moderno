372b4ca6d85aa38c6f0a30ab850aa1cfa72b8fbe
carlosyamakawa <carlos.yamakawa@gmail.com>
2020-05-15 17:51:31 -0400

    estruturas de control

---
+++ estruturas de controle/for1.js
@@ -0,0 +1,15 @@
+let contador = 1
+while (console <= 10){
+    console.log(`Contador = ${contador}`)
+    console++
+}
+
+for (let i=1;i<=10;i++) {
+    console.log(`i = ${i}`)
+}
+
+const notas = [6, 7, 9.8, 3, 5]
+
+for (i=0;i<notas.length;i++) {
+    console.log(`nota = ${notas[i]}`)
+}
