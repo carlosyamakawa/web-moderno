ea0e50583a7f609f8c3b55a5f2c7847c3ad47c21
carlosyamakawa <carlos.yamakawa@gmail.com>
2020-05-30 23:02:52 -0400

    OO part1

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural
+processamento(v1, v2, v3)
+
+// OO
+objeto = {
+    v1,
+    v2,
+    v3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento()
+
+// foco no objeto
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (é um)
+// 4. polimorfismo
